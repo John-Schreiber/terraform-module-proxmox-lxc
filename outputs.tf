a1c62064471085abf504dd72659090823a6edff7
John-Schreiber <56258966+John-Schreiber@users.noreply.github.com>
2024-06-04 14:34:46 -0400

    Update outputs.tf
    add private key

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "container_password" {
   value     = random_password.lxc_password.result
   sensitive = true
 }
+output "container_private_key" {
+  value     = tls_private_key.ubuntu_container_key.private_key_pem
+  sensitive = true
+}
