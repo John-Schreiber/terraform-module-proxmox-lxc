5cc07693c94b25c6bf0f6eac62fab28183ab004a
John Schreiber <jts@rosemontmarket.com>
2024-06-11 14:27:11 +0000

    remove ssh key output

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,3 @@ output "container_password" {
   value     = random_password.lxc_password.result
   sensitive = true
 }
-output "container_private_key" {
-  value     = tls_private_key.container_key.private_key_pem
-  sensitive = true
-}
